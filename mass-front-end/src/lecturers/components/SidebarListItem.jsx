040d837754a3d05e23a9b2042aa587e043cf9314
johnmuninga <johnmuninga03@gmail.com>
2023-10-01 15:45:22 +0200

    fix community

--- mass-front-end/src/lecturers/components/SidebarListItem.jsx
+++ mass-front-end/src/lecturers/components/SidebarListItem.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 function SidebarListItem({item}) {
   return (
-    <Link className='link' to={`/community/?roomId=${item.id}`}>
+    <Link className='link' to={`/communitylecturer/?roomId=${item.id}`}>
       <div className='sidebar__chat'>
         <div className='avatar__container'>
             <Avatar 
